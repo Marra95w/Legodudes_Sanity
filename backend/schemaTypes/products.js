a71d1732df422d2254190778c817cca4824599d9
Marra95w <maria.ulviksbakken@hiof.no>
2026-04-13 12:24:16 +0200

    La til mer kode

--- backend/schemaTypes/products.js
+++ backend/schemaTypes/products.js
@@ -9,6 +9,23 @@ const products = {
             type: "string"
         },
         {
+        title: 'Slug',
+        name: 'slug',
+        type: 'slug',
+        options: {
+            source: 'productname',
+            // maxLength: 200, // will be ignored if slugify is set
+            //Slugify function to convert input to a URL-friendly slug
+            slugify: input => input
+                                .toLowerCase()
+                                // Replace spaces with dashes
+                                .replace(/\s+/g, '-')
+                                // Remove regular expressions. Ta alle blank spaces og erstatte med bindestrek
+                                .slice(0, 200)
+                                // Gjør om tekstrengen til array, og kapper den. 200 lang
+        }
+        },       
+        {
             name: "price",
             title: "Pris",
             type: "number"
@@ -24,7 +41,7 @@ const products = {
             type: "image"
         },
         {
-            name: "category",
+            name: "productcategory",
             title: "Kategori",
             type: "reference",
             to: [
@@ -33,7 +50,28 @@ const products = {
                 }
             ]
         }
-    ]
+    ],
+    //preview et regelsett på hvordan innholdet vises
+    preview: {
+        //velge hvilke biter vi vil vise frem
+        select: {
+            title: 'productname',
+            inCat: 'productcategory.categoryname',
+            image: 'productimage'
+        },
+        //sanity funksjon
+        prepare(selection) {
+            //sender med selection, sender utvalget vi endrer på 
+            // = setttet med nøkkelsvar
+            const {title, inCat, image} = selection
+            return {
+                title: title, 
+                //liger under tittlen
+                subtitle: `Kategori: ${inCat ? inCat : "Ukjent"}`,
+                media: image
+            }
+        }
+    }
 }
 
 export default products
