0f62849afcb1636144c729b679ace94ad5b44c25
Marra95w <maria.ulviksbakken@hiof.no>
2026-04-13 14:03:40 +0200

    💯

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react'
 import ProductCard from './ProductCard'
+import client from '../helpers/clients'
 export default function Products({products, setCart}){
+  //lage en state, henter fra sanity
+
+    const [sanityProducts, setSanityProducts] = useState(null)
+
+    //react+sanity sammen 
+    //dokumentasjon fra internett -> servercomponents + clientcomponents 
+    //Mer ressurseffektiv måte => velge hva slags informasjon som er viktig å hente
+    //hvordan skrive asynkront 
+
+    //lage en klientlytter
+    //await -> vente på klienten, henter ved bruk av croq spørringer -> setter så inn i state
+    useEffect(() => {
+      async function fetchAllProducts() {
+        const allProducts = await client.fetch("*[_type == 'products']{_id, productname, price, 'category': productcategory->categoryname, 'imageURL': productimage.asset->url}")
+        setSanityProducts(allProducts)
+      }
+      fetchAllProducts()
+      //hvilke endringer skal vi bruke i useeffect
+    }, [])
+
+    console.log(sanityProducts)
+
     return (
     <div id="product-list">
-      {products.map(p => <ProductCard key={p.prodid} p={p} setCart={setCart} />)}
+      {sanityProducts?.map(p => <ProductCard key={p._id} p={p} setCart={setCart} />)}
       
     </div>)
   }
